a1b1f9d6b247645dc88bc3d02976b14d76b6776b
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-03-20 22:57:55 +0700

    update : add  addreview url

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -28,4 +28,32 @@ router.post("/getproductbyid", (req, res) => {
     })
 });
 
+router.post("/addreview", async(req, res) => {
+    const { review, productid, currentUser } = req.body;
+
+    const product = await Product.findById({ _id: productid })
+
+    const reviewModel = {
+        name: currentUser.name,
+        userid: currentUser._id,
+        rating: review.rating,
+        comment: review.comment
+    }
+
+    product.reviews.push(reviewModel);
+
+    // current value review / reviews length exist inside array
+    let rating = product.reviews.reduce((acc, val) => acc + val.rating, 0)/ product.reviews.length;
+    product.rating = rating;
+
+    product.save(err => {
+        if(err) {
+            return res.status(400).json({ message: 'Something Went Wrong' })
+        } else {
+            res.send('Review Submitted Successfully')
+        }
+    })
+
+})
+
 module.exports = router;
