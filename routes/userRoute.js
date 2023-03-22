5496768e6b8a1528c075e0a2a7f1c1da488c0011
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-03-23 06:16:31 +0700

    add bcrypt and user update route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+
+const bcrypt = require('bcrypt');
+const saltRound = 10;
 
 router.post("/register", (req, res) => {
 
@@ -10,42 +12,76 @@ router.post("/register", (req, res) => {
         if(docs.length > 0) {
             return res.status(400).json({ message: 'Something went wrong' })
         } else {
-            const newuser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password,
-            });
-        
-            newuser.save(err => {
-                if(!err) {
-                    res.send('User Registration Success')
-                } else {
-                    res.send('Something went wrong')
-                }
+
+            bcrypt.hash(req.body.password, saltRound, (err, hash) => {
+                const newuser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hash
+                });
+            
+                newuser.save(err => {
+                    if(!err) {
+                        res.send('User Registration Success')
+                    } else {
+                        res.send('Something went wrong')
+                    }
+                })
             })
+
+            if(err) {
+                return res.status(400).json({ message: 'Something went wrong' })
+            }
         }
 
-        if(err) {
-            return res.status(400).json({ message: 'Something went wrong' })
-        }
     })
 });
 
 router.post('/login', (req, res) => {
-    User.find({ email: req.body.email, password: req.body.password }, (err, docs) => {
+    User.find({ email: req.body.email }, (err, docs) => {
         if(docs.length > 0) {
-            const user = {
-                name: docs[0].name,
-                _id: docs[0]._id,
-                email: docs[0].email,
-                isAdmin: docs[0].isAdmin
-            }
-
-            res.send(user);
+            bcrypt.compare(req.body.password, docs[0].password, (err, result) => {
+                if(result === true) {
+                    const user = {
+                        name: docs[0].name,
+                        _id: docs[0]._id,
+                        email: docs[0].email,
+                        isAdmin: docs[0].isAdmin
+                    }
+            
+                    res.send(user);
+                } else {
+                    return res.status(400).json({ message: 'Invalid Credentials' });
+                }
+            });
         } else {
             return res.status(400).json({ message: 'Invalid Credentials' });
         }
     })
 });
+
+router.post('/update', (req, res) => {
+    const { userid, updateduser } = req.body;
+
+    bcrypt.hash(updateduser.password, saltRound, (err, hash) => {
+        if(err) {
+            return res.status(400).json({ message: err })
+        } else {
+            User.findByIdAndUpdate({_id: userid}, {
+                name: updateduser.name,
+                email: updateduser.email,
+                password: hash,
+            }, (err) => {
+                if(err) {
+                    return res.status(400).json({ message: "Something went wrong" })
+                } else {
+                    res.send('User details updated successfully')
+                }
+            })
+        }
+    })
+
+    
+})
 
 module.exports = router;
